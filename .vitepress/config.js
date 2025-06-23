946ea756bfa6a81b173231e8d0953f77affbbfff
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-06-23 13:42:21 +0200

    .

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -16,19 +16,19 @@ export default withMermaid(
                 {text: 'Home', link: '/'},
             ],
 
-            //sidebar: [
-            //    {text: 'Report',
-            //        items: [
-            //            {text: 'Process scoping meeting', link: `${reportPath}/1-process-scoping-meeting`},
-            //            {text: 'Valutazione proposte del committente', link: `${reportPath}/2-valutazione-proposte-committente`},
-            //            {text: 'Project Overview Statement', link: `${reportPath}/3-POS`},
-            //            {text: 'Analisi dei rischi', link: `${reportPath}/4-POS-AnalisiRischi`},
-            //            {text: 'Requirements Breakdown Structure', link: `${reportPath}/5-RBS`},
-            //            {text: 'PMLC Model', link: `${reportPath}/6-PMLC-model`},
-            //            {text: 'Traduzione', link: `${reportPath}/7-traduzione`},
-            //        ]
-            //    },
-            //],
+            sidebar: [
+                {text: 'Report',
+                    items: [
+                        {text: 'Process scoping meeting', link: `${reportPath}/1-prova`},
+                        //{text: 'Valutazione proposte del committente', link: `${reportPath}/2-valutazione-proposte-committente`},
+                        //{text: 'Project Overview Statement', link: `${reportPath}/3-POS`},
+                        //{text: 'Analisi dei rischi', link: `${reportPath}/4-POS-AnalisiRischi`},
+                        //{text: 'Requirements Breakdown Structure', link: `${reportPath}/5-RBS`},
+                        //{text: 'PMLC Model', link: `${reportPath}/6-PMLC-model`},
+                        //{text: 'Traduzione', link: `${reportPath}/7-traduzione`},
+                    ]
+                },
+            ],
 
             socialLinks: [
                 {icon: 'github', link: 'https://github.com/PPS-24-Papyrus/PM-Papyrus'}
